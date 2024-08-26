436642931951fb4099255c17536f9e918b3bd7b9
Emre Üçbudak <147994857+emreucbudak@users.noreply.github.com>
2024-08-26 19:57:09 +0300

    Add files via upload

---
+++ main (1).php
@@ -0,0 +1,12 @@
+<?php
+/******************************************************************************
+
+    Emre Üçbudak Karadeniz Teknik Üniversitesi
+
+*******************************************************************************/
+// PHP DIZI ICINDE DIZI TANIMLAMA
+$dizi = array();
+$dizi[0] = array("Emre Üçbudak","Ktü");
+$dizi[1] = array("2.sınıf","öğrencisi");
+print_r($dizi)
+?>
